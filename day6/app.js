917d51e8ca980b39e925a4942799dbaf1a2cada3
Magdalena Jadach <magdalena.jadach@gmail.com>
2017-01-29 16:40:56 +0000

    Add funcionality to filter matches

--- day6/app.js
+++ day6/app.js
@@ -3,5 +3,35 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 const cities = [];
 
 fetch(endpoint) //promise
-	.then(raw => raw.json()) //convert to json
-	.then(data => cities.push(...data)) //ES6 spread syntax
+    .then(raw => raw.json()) //convert to json
+    .then(data => cities.push(...data)) //ES6 spread syntax
+
+function findMatches(wordToFind, cities) {
+    return cities.filter(place => {
+        // here we need to figure out if the city or state matches what was searched
+        const regex = new RegExp(wordToFind, 'gi'); // global and case-insensitive search
+        return place.city.match(regex) || place.state.match(regex)
+    });
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${place.population}</span>
+      </li>
+    `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
