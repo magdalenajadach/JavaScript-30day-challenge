5924e5a0ac7f9239e6150ad01c66c5dc6862fb26
Magdalena Jadach <magdalena.jadach@gmail.com>
2017-03-04 19:10:34 +0000

    Finish day 10

---
+++ day10/app.js
@@ -0,0 +1,28 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+
+function handleCheck(event) {
+	// check if there is the shiht key down and that user is checking it
+	let isBetween = false;
+	let lastChecked;
+	if(event.shiftKey && this.checked) {
+		// loop over every single checkbox
+		checkboxes.forEach(checkbox => {
+			if (checkbox === this || checkbox === lastChecked) {
+				isBetween = !isBetween;
+			}
+
+			if (isBetween) {
+				checkbox.checked = true;
+			}
+		});
+	}
+
+	lastChecked = this;
+}
+
+checkboxes.forEach(function(checkbox) {
+	checkbox.addEventListener('click', handleCheck);
+}); // click event listen for mouse click and keybooard as well
+
+// arrowFuncion => checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
